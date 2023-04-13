bdf0e78b4119ded08d42549cfb10c6e3ddf87cdd
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2023-04-13 21:12:02 +0300

    added cors handling by cors library

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 const errorHandlingMiddleware = require('./middlewares/errorHandling');
 const celebrateErrorHandling = require('./middlewares/celebrateErrorHandling');
-const corsHandling = require('./middlewares/corsHandling');
+const { options } = require('./middlewares/corsOptions');
+// const corsHandling = require('./middlewares/corsHandling');
 // const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 
-app.use(corsHandling);
+app.use('*', cors(options));
 app.use(express.json());
 app.use(cookieParser());
 // app.use(requestLogger);
---
+++ middlewares/corsOptions.js
@@ -0,0 +1,12 @@
+const options = {
+  origin: [
+    'http://localhost:3000',
+  ],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
+  credentials: true,
+};
+
+module.exports = { options };
